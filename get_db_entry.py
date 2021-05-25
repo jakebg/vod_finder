afd27d6871bb2dd882a90f84ef8654855cc07000
jake <jake@localhost.localdomain>
2021-05-25 16:48:44 -0400

    adding date formatter

--- get_db_entry.py
+++ get_db_entry.py
@@ -1,8 +1,8 @@
 import mysql.connector
 from mysql.connector import Error
 from itertools import islice
-from mysql_vod import *
-
+#from mysql_vod import *
+from enum import Enum
 import re
 
 # September 4, 2020 (1/2)
@@ -14,63 +14,88 @@ import re
 # [0-9]{2}\/[0-9]{2}\/[0-9]{4}
 
 re_date1 = "[0-9]{2}\/[0-9]{2}\/[0-9]{4}"
-re_date2 = '((January)?|(February)?|(March)?|(April)?|(May)?|(June)?|(July)?|(August)?|(September)?|(October)?|(November)?|(December)?)?\D?(\d{1,2}\D?)?\D?(19[7-9]\d|20\d{2})\s?(\(\d{1,2}\/\d\))?'
+# add \s?(\(\d{1,2}\/\d\))? for parts of videos
+re_date2 = '((January)?|(February)?|(March)?|(April)?|(May)?|(June)?|(July)?|(August)?|(September)?|(October)?|(November)?|(December)?)?\D?(\d{1,2}\D?)?\D?(19[7-9]\d|20\d{2})'
 re_list = [re_date1, re_date2]
 
 filename = 'test_get_db.txt'
-game_list = ['Grand Theft Auto V', 'Skyrim', 'Tony Hawk\'s Pro Skater']
-
-
+game_list = ['Grand Theft Auto V', 'Skyrim', 'Tony Hawk\'s Pro Skater','Animal Crossing']
 
 
 def parse_title(parse_line):
     print(parse_line)
 
     if any((match := string) in parse_line for string in game_list):
-        print('Game: ', match)
-        game_result = 'yes'
+        game_result = match
     else:
         game_result = 'NOGAME'
-        print('NO GAME')
 
     for x in re_list:
         result = re.search(x,parse_line)
         if result:
-            date = result.group(0)
-            print('Date found: ', date)
+            date_result = result.group(0)
             break;
         else:
-            date = 'NODATE'
+            date_result = 'NODATE'
             #print('Date not found.')
 
+    # print('Game: ', game_result)
+    # print('Date: ', date_result)
 
-    #result = re.search(date_pattern,parse_line)
-    # result = False
-    # if result:
-    #     date = result.group(0)
-    #     print('Date found: ', date)
-    # else:
-    #     date = 'NODATE'
-    #     print('Date not found.')
-    print('\n')
+    return date_result, game_result
 
 
-    return date, game_result
-
-# [Grand Theft Auto V] "NoPixel | Yung Dab - !EU" (03/11/2019)
 def read_from_db_file():
     query = '''SELECT vod_title FROM vod;'''
 
     test = read_query(connection, query)
     print(type(test))
+
     with open(filename, 'w') as f:
         for listitem in test:
             f.write('%s\n'%listitem)
-    pass
+
+# YYYY-MM-DD
+# April 19, 2020
+# 05/24/2019
+def change_date_format(date):
+    import datetime
+
+    if re.search(re_date1,date):
+        # FORM OF 05/24/2019
+        date_time_obj = datetime.datetime.strptime(date, '%m/%d/%Y')
+        new_date = date_time_obj.date()
+
+    elif re.search(re_date2,date):
+        # FORM OF April 19, 2020
+        date_time_obj = datetime.datetime.strptime(date, '%B %d, %Y')
+        new_date = date_time_obj.date()
+
+    else:
+        print('Date format not found')
+        new_date = 'NODATE'
+
+    return new_date
+
 
 if __name__ == '__main__':
+
     with open(filename,'r') as f:
         for line in f:
-            parse_title(line.strip())
+            date, game = parse_title(line.strip())
+            
+            date = change_date_format(date)
+            print('Game: ', game)
+            print('Date: ', date,'\n')
 
-    pass
+    # test_string = '[Grand Theft Auto V] "NoPixel | Yung Dab - !EU" (03/11/2019)'
+    # date, game = parse_title(test_string.strip())
+    # date= change_date_format(date)
+    # print('Game: ', game)
+    # print('Date: ', date,'\n')
+    # test_string2 = 'Clint Stevens - Skyrim (Part 6) [March 18, 2020]'
+    # date, game = parse_title(test_string2.strip())
+    # date = change_date_format(date)
+    # print('Game: ', game)
+    # print('Date: ', date,'\n')
+    
